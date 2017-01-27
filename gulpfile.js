136bcc0a59d4e586cc2f88a050817f02dc3249d8
aaron.harpt <aaron.harpt@gmail.com>
2017-01-27 15:11:40 -0600

    Wrote gulpfile (for now)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
 var gulp = require("gulp");
+var marked = require("gulp-marked");
+var sass = require("gulp-sass");
 
 gulp.task("marked", function() {
-  {
+  gulp.src("./src/*.md")
+  .pipe(marked({
     gfm: true
-  }
+  }))
+  .pipe(gulp.dest("./html/"))
+});
+
+gulp.task("sass", function() {
+return gulp.src("./scss/.scss")
+.pipe(sass())
+.pipe(gulp.dest("./css"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("./scss/.scss", ["sass"])
+  gulp.watch("./src/*.md", ["marked"]);
+});
+
+gulp.task("default", ["watch"] {
 });
