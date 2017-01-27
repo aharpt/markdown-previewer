953cfb4dfa9e13eb5307f82a71664e2e47342b6e
aaron.harpt <aaron.harpt@gmail.com>
2017-01-27 15:04:05 -0600

    Planned how to write markdown-previewer

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
+// make markdown file the input field and make the state all of the text
+// changing the input sould change the state to the input's value
 
+/* as I change the markdown, (keyup) change the
+text to the right to display the new value */
 
-
+/* all rendered markdown should be in a single state.
+when the input is changed, that should change the
+html in the file, which in turn should trigger a state rerender
+which will change the content shown
+*/
 
 var Main = React.createClass({
 
